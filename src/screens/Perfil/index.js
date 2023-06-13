ba6675248a5a4cb9ed8b4f066f1777ccb9428b56
eugreg <daiaegregory@gmail.com>
2023-06-13 15:08:13 -0300

    criacao de um bottom tab

---
+++ src/screens/Perfil/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View,ScrollView,TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
+
+export default function Perfil() {
+  return (
+    <View style={styles.container}>
+      <Text>Perfil</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
